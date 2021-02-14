ba928f16193edaae71aa885e9c1fa15f1b9d8744
jeswr <jesse.wright@anu.edu.au>
2021-02-14 21:50:31 +1100

    WIP: fix type casting

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,23 @@
 import { useReducer } from 'react';
 
+function castIdentity<T, K extends T>(value: K): T {
+  return value;
+}
+
 /**
  * useState implemented using the useReducer API
  * @param init The initial value of the function
  * @param modification Modifies the action value
  */
-export function useState<T>(init: T): [T, (action: T) => void];
-export function useState<K, T>(init: T, modification: (param: K) => T): [T, (action: K) => void];
-export function useState<K, T>(init: T, modification?: (param: K) => T): [T, (action: K) => void] {
+export function useState<T, K extends T>(init: T): [T, (action: K) => void];
+export function useState<T, K>(init: T, modification: (param: K) => T): [T, (action: K) => void];
+export function useState<T, K>(init: T, modification?: (param: K) => T): [T, (action: K) => void] {
   if (modification) {
     return useReducer<(state: T, action: K) => T>(
-      (state, action) => modification(action),
+      (_, action) => modification(action),
     init);
   }
   return useReducer<(state: T, action: K) => T>(
-    (state, action) => action as unknown as T,
+    (_, action: K) => castIdentity<T, K>(action),
   init);
 }
